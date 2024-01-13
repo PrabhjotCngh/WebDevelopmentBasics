c07156e43606e0fc19079967c63ab7aef7200cbd
Prabjot Singh <prabjot.singh@andersencorp.com>
2024-01-13 18:57:15 -0500

    Dynamic route with URL parameters, programatic navigation with useNavigate, Implemented reading and setting of query string

--- React/11-worldwise/src/components/Map.jsx
+++ React/11-worldwise/src/components/Map.jsx
@@ -1,6 +1,26 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const navigate = useNavigate();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
+      <h1>Map</h1>
+      <h2>
+        Postions: {lat}, {lng}
+      </h2>
+    </div>
+  );
 }
 
 export default Map;
