9f17069111ac5bcca5db5256c81aeed059727419
Prabjot Singh <prabjot.singh@andersencorp.com>
2024-01-13 17:47:36 -0500

    Building pages, nested routes and index route

---
+++ React/11-worldwise/src/components/CityList.jsx
@@ -0,0 +1,27 @@
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import CityItem from "./CityItem";
+import PropTypes from "prop-types";
+
+CityList.propTypes = {
+  cities: PropTypes.array,
+  isLoading: PropTypes.bool,
+};
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+  return (
+    <div className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </div>
+  );
+}
+
+export default CityList;
