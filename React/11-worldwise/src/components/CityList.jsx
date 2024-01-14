0d5a6148d3660522ed364b5b6a82c0f56c6fcc21
Prabjot Singh <prabjot.singh@andersencorp.com>
2024-01-14 13:50:10 -0500

    Context API implementation to share global state

--- React/11-worldwise/src/components/CityList.jsx
+++ React/11-worldwise/src/components/CityList.jsx
@@ -3,13 +3,16 @@ import Spinner from "./Spinner";
 import Message from "./Message";
 import CityItem from "./CityItem";
 import PropTypes from "prop-types";
+import { useCities } from "../contexts/CitiesContext";
 
 CityList.propTypes = {
   cities: PropTypes.array,
   isLoading: PropTypes.bool,
 };
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return (
