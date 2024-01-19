92c10986eeb9498f71eb203967aa425a476bc6b9
Prabjot Singh <prabjot.singh@andersencorp.com>
2024-01-19 09:46:08 -0500

    Project setup and Implemented styled components

---
+++ React/16-the-wild-oasis/src/App.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.main`
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <div>
+              <Heading as="h1">Check in and out</Heading>
+              <Heading as="h2">The Wild Oasis</Heading>
+
+              <Button onClick={() => alert("alert")}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("alert")}
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
