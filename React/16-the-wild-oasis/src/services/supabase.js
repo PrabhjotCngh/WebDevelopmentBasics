23831717e43b482f7c84de741f7f9819f21d27ef
Prabjot Singh <prabjot.singh@andersencorp.com>
2024-01-23 11:24:53 -0500

    Added supabase library

---
+++ React/16-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Create a single supabase client for interacting with your database
+const supabaseUrl = "https://czphyscxkdgxchfumwzp.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6cGh5c2N4a2RneGNoZnVtd3pwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2OTA0NzcsImV4cCI6MjAyMTI2NjQ3N30.lamViBdF41ByC7RTHAPk9nCgF7089IANenCZS5Z9hmg";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
