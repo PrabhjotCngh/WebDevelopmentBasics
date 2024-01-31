c2e5495a1ecd53e6a7eabbcc2954b435e90b4a1b
Prabjot Singh <prabjot.singh@andersencorp.com>
2024-01-31 16:55:57 -0500

    Optimised pagination in booking page

--- React/16-the-wild-oasis/src/features/bookings/BookingTableOperations.jsx
+++ React/16-the-wild-oasis/src/features/bookings/BookingTableOperations.jsx
@@ -1,4 +1,4 @@
-//import SortBy from 'ui/SortBy';
+import SortBy from "../../ui/SortBy";
 import Filter from "../../ui/Filter";
 import TableOperations from "../../ui/TableOperations";
 
@@ -16,17 +16,17 @@ function BookingTableOperations() {
         ]}
       />
 
-      {/* <SortBy
+      <SortBy
         options={[
-          { value: 'startDate-desc', label: 'Sort by date (recent first)' },
-          { value: 'startDate-asc', label: 'Sort by date (earlier first)' },
+          { value: "startDate-desc", label: "Sort by date (recent first)" },
+          { value: "startDate-asc", label: "Sort by date (earlier first)" },
           {
-            value: 'totalPrice-desc',
-            label: 'Sort by amount (high first)',
+            value: "totalPrice-desc",
+            label: "Sort by amount (high first)",
           },
-          { value: 'totalPrice-asc', label: 'Sort by amount (low first)' },
+          { value: "totalPrice-asc", label: "Sort by amount (low first)" },
         ]}
-      /> */}
+      />
     </TableOperations>
   );
 }
